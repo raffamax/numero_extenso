a74f0dbe69dac7660fe4b848a355f3f2d304924d
Rafa Max <rafaelmaxguitar@gmail.com>
2023-04-20 02:05:14 -0300

    add numbers and some css

--- scripts.js
+++ scripts.js
@@ -1,14 +1,17 @@
-let unidade = ['Zero','Um','Dois','Três','Quatro','Cinco','Seis','Sete','Oito','Nove','Dez','Onze','Doze','Treze','Quatorze','Quinze','Dezesseis','Dezessete','Dezoito','Dezenove'];
-let dezenaDezena = ['','','Vinte','Trinta','Quarenta','Cinquenta','Sessenta','Setenta','Oitenta','Noventa'];
-let centena = ['Cem','Cento','Duzentos','Trezentos','Quatrocentos','Quinhentos','Seiscentos','Setecentos','Oitocentos','Novecentos'];
+let unidade = ['zero','um','dois','três','quatro','cinco','seis','sete','oito','nove','dez','onze','doze','treze','quatorze','quinze','dezesseis','dezessete','dezoito','dezenove'];
+let dezenaDezena = ['','','vinte','trinta','quarenta','cinquenta','sessenta','setenta','oitenta','noventa'];
+let centena = ['cem','cento','duzentos','trezentos','quatrocentos','quinhentos','seiscentos','setecentos','sitocentos','novecentos'];
+
+
 
 
 function enviar() {
   let number = Number(document.getElementById('number').value);
 
-  if (number < 0 || number > 100000) {
+  if (number < 0 || number > 100999) {
     document.getElementById('resultNegative').innerHTML = `Este número está fora do intervalo q sei escrever, Consulte novamente em breve.`;
-    document.getElementById('result').innerHTML = ''; 
+    document.getElementById('result').innerHTML = '';
+    document.getElementById('text-result').innerHTML = '';
   } else {
     document.getElementById('resultNegative').innerHTML = '';
     
@@ -32,12 +35,12 @@ function enviar() {
         
 
       if (number === 1000) {
-        return 'Mil' ;
+        return 'mil' ;
       }
 
       if (number < 2000) {
-        return 'Mil' + 
-        (number % 1000 === 100 ? ' e Cem' : '' ) +
+        return 'mil' + 
+        (number % 1000 === 100 ? ' e cem' : '' ) +
         (number % 1000 > 100 ? ', ' + centena[Math.floor((number % 1000) / 100)] : '') +
         (number % 100 < 20 && number % 100 > 0 ? ' e ' + unidade[number % 100] : '') +
         (number % 100 < 100 && number % 100 >= 20 ? ' e ' + dezenaDezena[Math.floor((number % 100) / 10)] : '') +
@@ -46,7 +49,7 @@ function enviar() {
     
       if (number < 20000) {
         return unidade[Math.floor(number / 1000)] + ' mil' +
-        (number % 1000 === 100 ? ' e Cem' : '' ) +
+        (number % 1000 === 100 ? ' e cem' : '' ) +
         (number % 1000 > 100 ? ', ' + centena[Math.floor((number % 1000) / 100)] : '') +
         (number % 100 < 20 && number % 100 > 0 ? ' e ' + unidade[number % 100] : '') +
         (number % 100 < 100 && number % 100 >= 20 ? ' e ' + dezenaDezena[Math.floor((number % 100) / 10)] : '') +
@@ -56,7 +59,7 @@ function enviar() {
       if (number < 100000) {
         return dezenaDezena[Math.floor(number / 10000)] + 
         (Math.floor(number / 1000) % 10 > 0 && Math.floor(number / 1000) % 10 < 10 ? ' e ' + unidade[Math.floor(number / 1000) % 10] + ' mil' : ' mil') +
-        (number % 1000 === 100 ? ' e Cem' : '' ) +
+        (number % 1000 === 100 ? ' e cem' : '' ) +
         (number % 1000 > 100 ? ', ' + centena[Math.floor((number % 1000) / 100)] : '') +
         (number % 100 < 20 && number % 100 > 0 ? ' e ' + unidade[number % 100] : '') +
         (number % 100 < 100 && number % 100 >= 20 ? ' e ' + dezenaDezena[Math.floor((number % 100) / 10)] : '') +
@@ -67,13 +70,45 @@ function enviar() {
         return centena[Math.floor(number % 100000)] + ' mil' ;
       }
 
-      
+      if (number < 101000) {
+        return  'cem mil' +
+        dezenaDezena[Math.floor(number / 100000)] + 
+        (Math.floor(number / 1000) % 10 > 0 && Math.floor(number / 1000) % 10 < 10 ? ' e ' + unidade[Math.floor(number / 1000) % 10] + ' mil' : '') +
+        (number % 1000 === 100 ? ' e cem' : '' ) +
+        (number % 1000 === 200 ? ' e cuzentos' : '' ) +
+        (number % 1000 === 300 ? ' e trezentos' : '' ) +
+        (number % 1000 === 400 ? ' e quatrocentos' : '' ) +
+        (number % 1000 === 500 ? ' e quinhentos' : '' ) +
+        (number % 1000 === 600 ? ' e seiscentos' : '' ) +
+        (number % 1000 === 700 ? ' e setecentos' : '' ) +
+        (number % 1000 === 800 ? ' e oitocentos' : '' ) +
+        (number % 1000 === 900 ? ' e novecentos' : '' ) +
+        (number % 1000 > 100 
+          && number % 1000 !== 200
+          && number % 1000 !== 300
+          && number % 1000 !== 400
+          && number % 1000 !== 500
+          && number % 1000 !== 600
+          && number % 1000 !== 700
+          && number % 1000 !== 800
+          && number % 1000 !== 900 ? ', ' + centena[Math.floor((number % 1000) / 100)] : '') +
+        (number % 100 < 20 && number % 100 > 0 ? ' e ' + unidade[number % 100] : '') +
+        (number % 100 < 100 && number % 100 >= 20 ? ' e ' + dezenaDezena[Math.floor((number % 100) / 10)] : '') +
+        (number % 100 > 20 && (((number % 100) % 100) % 10) !== 0 ? ' e ' + unidade[((number % 100) % 100) % 10] : '' ) ;
+      }
+
+
 
     }
 
-    console.log(Math.floor(number / 100000) - 1)
+    console.log(Math.floor(number % 100000))
+
     let numberByText = transformNumberText(number);
     let result = document.getElementById('result');
-    result.innerHTML = `O número digitado foi... ${numberByText}`;
+    result.innerHTML = `${numberByText}`;
+
+    let textResult = document.getElementById('text-result');
+    textResult.innerHTML = 'O número digitado foi...' ;
   }
 }
+

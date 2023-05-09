7c8bd95e2687aa48857d3786d2969907d034630f
Rafa Max <rafaelmaxguitar@gmail.com>
2023-05-08 23:56:59 -0300

    some corrections and add logic

--- scripts.js
+++ scripts.js
@@ -3,8 +3,6 @@ let dezenaDezena = ['','','vinte','trinta','quarenta','cinquenta','sessenta','se
 let centena = ['cem','cento','duzentos','trezentos','quatrocentos','quinhentos','seiscentos','setecentos','sitocentos','novecentos'];
 
 
-
-
 function enviar() {
   let number = Number(document.getElementById('number').value);
 
@@ -99,9 +97,9 @@ function enviar() {
 
       if (number < 1000000) {
         return centena[Math.floor(number / 100000)] +
-        ((number / 1000) % 100 < 20 && (number / 1000) % 100 > 0 ? ' e ' + unidade[Math.floor(number / 1000) % 100] + ' mil ' : '' ) +
-        ((number / 1000) % 100 < 100 && (number / 1000) % 100 >= 20 ? ' e ' + dezenaDezena[Math.floor(((number / 1000) % 100) / 10)] : '' ) +
-        (Math.floor(((number / 1000) % 100) % 10 ) !== 0 ? ' e ' + unidade[Math.floor(((number / 1000) % 100) % 10 )] + ' mil' : ' mil') +
+        (Math.floor(number / 1000) % 100 < 20 && Math.floor(number / 1000) % 100 > 0 ? ' e ' + unidade[Math.floor(number / 1000) % 100] : '' ) +
+        (Math.floor(number / 1000) % 100 < 100 && Math.floor(number / 1000) % 100 >= 20 ? ' e ' + dezenaDezena[Math.floor(((number / 1000) % 100) / 10)] : '' ) +
+        (Math.floor((number / 1000) % 100) % 10 !== 0 && Math.floor(number / 1000) % 100 > 20 ? ' e ' + unidade[Math.floor(((number / 1000) % 100) % 10)] + ' mil ' : ' mil ') +
         (number % 1000 === 100 ? ' e cem' : '' ) +
         (number % 1000 === 200 ? ' e cuzentos' : '' ) +
         (number % 1000 === 300 ? ' e trezentos' : '' ) +
@@ -129,15 +127,42 @@ function enviar() {
         return 'Um milhão'
       }
 
-      // if (number < 1000000000) {
-      //   return 
-      // }
+      if (number < 2000000) {
+        return 'Um milhão' +
+
+
+        
+        (Math.floor(number / 1000) % 100 < 20 && Math.floor(number / 1000) % 100 > 0 ? ' e ' + unidade[Math.floor(number / 1000) % 100] + ' mil' : '' ) +
+        (Math.floor(number / 1000) % 100 < 100 && Math.floor(number / 1000) % 100 >= 20 ? ' e ' + dezenaDezena[Math.floor(((number / 1000) % 100) / 10)] : '' ) +
+        (Math.floor((number / 1000) % 100 ) % 10 !== 0 && Math.floor(number / 1000) % 100 > 20 ? ' e ' + unidade[Math.floor(((number / 1000) % 100) % 10)] + ' mil' : '' ) +
+        (number % 1000 === 100 ? ' e cem' : '' ) +
+        (number % 1000 === 200 ? ' e cuzentos' : '' ) +
+        (number % 1000 === 300 ? ' e trezentos' : '' ) +
+        (number % 1000 === 400 ? ' e quatrocentos' : '' ) +
+        (number % 1000 === 500 ? ' e quinhentos' : '' ) +
+        (number % 1000 === 600 ? ' e seiscentos' : '' ) +
+        (number % 1000 === 700 ? ' e setecentos' : '' ) +
+        (number % 1000 === 800 ? ' e oitocentos' : '' ) +
+        (number % 1000 === 900 ? ' e novecentos' : '' ) +
+        (number % 1000 > 100 
+          && number % 1000 !== 200
+          && number % 1000 !== 300
+          && number % 1000 !== 400
+          && number % 1000 !== 500
+          && number % 1000 !== 600
+          && number % 1000 !== 700
+          && number % 1000 !== 800
+          && number % 1000 !== 900 ? ', ' + centena[Math.floor((number % 1000) / 100)] : '') +
+        (number % 100 < 20 && number % 100 > 0 ? ' e ' + unidade[number % 100] : '') +
+        (number % 100 < 100 && number % 100 >= 20 ? ' e ' + dezenaDezena[Math.floor((number % 100) / 10)] : '') +
+        (number % 100 > 20 && (((number % 100) % 100) % 10) !== 0 ? ' e ' + unidade[((number % 100) % 100) % 10] : '' ) ;
+      }
 
 
 
     }
 
-    console.log(number / 1000000)
+    console.log((number / 1000) % 100 )
 
     let numberByText = transformNumberText(number);
     let result = document.getElementById('result');
